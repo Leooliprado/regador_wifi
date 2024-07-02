7f36a1cc7b9096053810f90cada5de78fbb327bb
Leooliprado <leonardooliveiraprado2005@gmail.com>
2024-07-02 20:11:48 -0300

    leo0207242011

---
+++ banco_de_dados.py
@@ -0,0 +1,29 @@
+import psycopg2
+from psycopg2 import sql
+
+
+# Configurações do banco de dados
+db_config = {
+    'dbname': '',
+    'user': '',
+    'password': '',
+    'host': '',
+    'port': ''
+}
+
+
+
+# Função para conectar ao banco de dados
+def connect_db():
+    conn = psycopg2.connect(**db_config)
+    return conn
+
+# Função para inserir dados no banco de dados
+def insert_data(data, umidade_solo, precisa_irrigar):
+    conn = connect_db()
+    cur = conn.cursor()
+    query = sql.SQL("INSERT INTO inrrigar (data, umidade_solo, precisa_irrigar) VALUES (%s, %s, %s)")
+    cur.execute(query, (data, umidade_solo, precisa_irrigar))
+    conn.commit()
+    cur.close()
+    conn.close()
--- regador_sever.py
+++ regador_sever.py
@@ -1,18 +1,33 @@
+from datetime import datetime
 from flask import Flask, request
 import socket
+from psycopg2 import sql
+import schedule
+import time
+from threading import Thread
+
+from banco_de_dados import insert_data  # Corrigido o import para a função insert_data
+from data_e_hora import dataehora
 
 app = Flask(__name__)
 
 # Variável global para armazenar a umidade
 umidade = None
 
-# arduino
+
+# Rota para receber dados de umidade do Arduino
 @app.route('/umidade', methods=['POST'])
 def coloca_umidade():
     global umidade
     dados = request.form.get("dados")
     if dados is not None:
-        umidade = dados
+        umidade = float(dados)
+        precisa_irrigar = umidade > 3001  # Ajustado o valor de comparação da umidade
+
+        data = dataehora()  # Obtem a data e hora atual
+        
+        # Inserir os dados no banco de dados
+        insert_data(data, umidade, precisa_irrigar)
 
         print("\033[34m************************>> RECEBI <<************************\n\033[0m")
         print(f'\033[92m Umidade recebida: {dados}\n\033[0m') 
@@ -23,7 +38,7 @@ def coloca_umidade():
         print('\033[91mDados não fornecidos\033[0m')  
         return 'Dados não fornecidos', 400
 
-# celular
+# Rota para solicitar dados de umidade
 @app.route('/puxar', methods=['GET'])
 def puxa_umidade():
     global umidade
@@ -37,9 +52,21 @@ def puxa_umidade():
         print('\033[91m Umidade não disponível\033[0m')  
         return 'Umidade não disponível', 404
 
+
+# Função para executar as tarefas agendadas
+def run_schedule():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
 if __name__ == '__main__':
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
     print(f"Operando no IP: {ip}")
+
+    # Iniciar a thread para enviar umidade ao banco de dados a cada 30 minutos
+    umidade_thread = Thread(target=run_schedule)  # Corrigido o target da thread para run_schedule
+    umidade_thread.daemon = True
+    umidade_thread.start()
 
     app.run(host=ip, port=4000, debug=False)
